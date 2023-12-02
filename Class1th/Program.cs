34bd1cb684766ec3caf9f36436d86387b277e494
jinhee <140380451+parkjinhee1234@users.noreply.github.com>
2023-12-02 16:41:53 +0900

    2023.12.2 변수의 선언

---
+++ Class1th/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1th
+{
+    class Program
+    {
+        //static void Main(string [] args)
+        // 프로그램의 진입점 역할을 수행하는 함수입니다.
+        static void Main(string[] args)
+        {
+            //Console.WriteLine() : 특정한 문자열을 출력하는 함수입니다.
+
+            #region 자료형
+            // 데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다.
+
+            // 자료형은 각각의 자료형마다 크기가 정해져 있으며, 자료형의 크기는
+            // 바이트 단위로 이루어져 있습니다.
+
+            // 자료형의 경우 자료형에 따라 저장할 수 있는 값의 종류와 범위가 결정됩니다.
+            #endregion
+            // 데이터를 저장할 수 있는 메모리 공간을 생성하는 것입니다.
+
+            bool boolean = true;   //  bool  :   1 byte
+                                                
+            char character = 'A';   //  char    : 1 byte
+                                                
+                                                
+            short shortData = 100;  //  shrot   : 2 byte
+                                                
+            int integer = 100;     //  int   :   4 byte
+                                                
+            float health  = 3.15f;   //  float   : 4 byte
+                                                
+            double attack = 14.5;    // double   : 8 byte
+                                               
+            // 변수를 생성하게 되면 변수는 메모리 공간을 가지게 됩니다.
+            Console.WriteLine("boolean 변수의 값 : "  + boolean);
+            Console.WriteLine("character 변수의 값 : "  + boolean);
+            Console.WriteLine("integer 변수의 값 : "  + boolean);
+            Console.WriteLine("health 변수의 값 : "  + boolean);
+            Console.WriteLine("attack 변수의 값 : "  + boolean);
+
+
+
+            // 변수의 메모리 공간은 프로그램이 실행될 때마다 바뀌며, 여러 개의
+            // 변수가 있을 때 서로 고유의 메모리 공간을 가지고 있습니다.
+
+            boolean = false;
+            character = '8';
+            integer = 50;
+            health = 1.87f;
+            attack = 99.8;
+
+
+            // 메모리 공간으로 해당 메모리에 저장된 값을 참조할 수 있으며,
+            // 변수를 생성하기 위해 메모리 공간의 크기를 지정해주는 자료형을
+            // 먼저 선언해주어야 합니다.
+
+            Console.WriteLine("boolean 변수의 값 : " + boolean);
+            Console.WriteLine("character 변수의 값 : " + boolean);
+            Console.WriteLine("integer 변수의 값 : " + boolean);
+            Console.WriteLine("health 변수의 값 : " + boolean);
+            Console.WriteLine("attack 변수의 값 : " + boolean);
+
+            // 변수는 프로그램이 실행되는 도안 값을 바꿀 수 있으며,
+            // 원래 저장되어 있는 값은 새로 저장되는 값에 의해 지워집니다.
+
+            Console.WriteLine("C# 언어");
+
+            
+        }
+    }
+}
